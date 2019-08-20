b4d9c422d10cdb32a05b9273f35b0d312a532301
jhuang30 <jieyinghuang30@gmail.com>
2019-08-20 11:29:13 -0400

    userModel

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World again"));
+app.get("/", (req, res) => {
+  console.log(res);
+  res.send("Hello World again");
+});
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
